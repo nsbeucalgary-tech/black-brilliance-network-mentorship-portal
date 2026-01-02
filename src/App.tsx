50ae5f516b4769feff8417eed76ae0435b13c734
fisayoadabs <fisayoadabs@gmail.com>
2026-01-02 06:45:24 -0700

    chore: initialization of database connection to web app. Created example pages as well as dummy crud for understanding.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/Landing";
+import ExamplePage from "./_db_controller/example/page";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<ExamplePage />} />
             </Routes>
         </Router>
     );
