cb55660626f53e14e9e6f331696ea40a756051c9
kihyren <obinnaezejoshua@gmail.com>
2026-03-29 20:32:46 -0600

    Removing code in LoggedInLayout.tsx that let the matching page ignore the default navbar

--- src/components/LoggedInLayout.tsx
+++ src/components/LoggedInLayout.tsx
@@ -1,17 +1,8 @@
-import { Outlet, useLocation } from "react-router-dom";
-import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 
 export default function LoggedInLayout() {
-  const location = useLocation();
-
-  // Any route that starts with /matching will be treated as the "fullscreen" page
-  // This hides the Navbar and lets the Outlet take full width, so the matching page matches the figma design
-  const isMatchingPage = location.pathname.startsWith("/matching");
-
   return (
     <div className="flex w-full">
-      {/* Hide the Navbar on Matching so the page can be fullscreen */}
-      {!isMatchingPage && <Navbar />}
 
       {/* If Navbar is hidden, let Outlet take full width. Otherwise, it sits beside Navbar. */}
       <div className="flex-1 min-w-0">
