ca476947ba4337a238d689aa482aa69721a7d882
fisayoadabs <fisayoadabs@gmail.com>
2026-05-16 04:46:27 -0600

    docs: Create a function definition comment template.

---
+++ funtionDefinitionTemplate.ts
@@ -0,0 +1,10 @@
+// FUNCTION DEFINITION TEMPLATE
+/**
+ * [Short summary sentence]
+ *
+ * [Optional extra explanation / important context]
+ *
+ * @param paramName - Description
+ * @param anotherParam - Description
+ * @returns Description of returned value
+ */
